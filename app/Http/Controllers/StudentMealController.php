6824fba2bc5563ad0c35ee3f9d4b0da169f55874
Ammar Midani <ammar.midani.1994@gmail.com>
2025-04-26 11:59:24 +0300

    add Model, Factory, migration, Seeder, Requests, Controller, Policy

---
+++ app/Http/Controllers/StudentMealController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentMeal;
+use App\Http\Requests\StoreStudentMealRequest;
+use App\Http\Requests\UpdateStudentMealRequest;
+
+class StudentMealController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudentMealRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentMeal $studentMeal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentMeal $studentMeal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudentMealRequest $request, StudentMeal $studentMeal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentMeal $studentMeal)
+    {
+        //
+    }
+}
---
+++ app/Models/StudentMeal.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentMeal extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentMealFactory> */
+    use HasFactory;
+}
