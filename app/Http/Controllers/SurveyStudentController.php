25a0bd611fba4c08120284cc8aeeb629b5f06180
Ammar Midani <ammar.midani.1994@gmail.com>
2025-04-29 17:05:08 +0300

    add Model, Factory, migration, Seeder, Requests, Controller, Policy

---
+++ app/Http/Controllers/SurveyStudentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SurveyStudent;
+use App\Http\Requests\StoreSurveyStudentRequest;
+use App\Http\Requests\UpdateSurveyStudentRequest;
+
+class SurveyStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSurveyStudentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SurveyStudent $surveyStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SurveyStudent $surveyStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSurveyStudentRequest $request, SurveyStudent $surveyStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SurveyStudent $surveyStudent)
+    {
+        //
+    }
+}
