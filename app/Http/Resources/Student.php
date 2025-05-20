6714cc79027f6903a48277e497035c91439bfd0b
Ammar Midani <ammar.midani.1994@gmail.com>
2025-05-21 00:25:25 +0300

    update Resources

--- app/Http/Resources/Student.php
+++ app/Http/Resources/Student.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\GenderType;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,23 @@ class Student extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $gender_type = GenderType::fromValue($this->gender);
+
+        return [
+            'student_number' => $this->student_number,
+            'name' => $this->name,
+            'gender' => [
+                'key' => $gender_type->key,
+                'name' => $gender_type->description,
+                'value' => $gender_type->value,
+            ],
+            'qr_code' => $this->qr_code,
+            'active' => $this->is_active,
+            'date_from' => $this->date_from,
+            'date_to' => $this->date_to,
+            'nationality' => Nationality::make($this->nationality),
+            'created_at' => date('Y-m-d', strtotime($this->created_at)),
+            'updated_at' => date('Y-m-d', strtotime($this->updated_at)),
+        ];
     }
 }
