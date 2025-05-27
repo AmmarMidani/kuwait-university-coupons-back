9ce36546d9d4bbb299adb4d4dbc7b8dc03ed1f87
Ammar Midani <ammar.midani.1994@gmail.com>
2025-05-27 19:17:13 +0300

    add login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,70 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('pagename')
+Login
+@endsection
+
+@section('css')
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!-- Login -->
+<div class="card p-md-7 p-1">
+    <!-- Logo -->
+    <div class="app-brand justify-content-center mt-5">
+        <a href="{{route('welcome')}}" class="app-brand-link gap-2">
+            <img src="{{ asset('assets/img/favicon/favicon.ico') }}?v={{ config('app.version') }}" alt="">
+            <span class="app-brand-text demo text-heading fw-semibold">Kuwait University</span>
+        </a>
+    </div>
+    <!-- /Logo -->
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <div class="card-body mt-1">
+        <h4 class="mb-1">Welcome to {{ config('app.name', 'Laravel') }}! 👋</h4>
+        <p class="mb-5">Please sign-in to your account</p>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+        <form id="formAuthentication" class="mb-5" action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="form-floating form-floating-outline mb-5">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
+                    autocomplete="email" required value="{{ old('email') }}" placeholder="Enter your email" autofocus />
+                <label for="email">Email</label>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div class="mb-5">
+                <div class="form-password-toggle">
+                    <div class="input-group input-group-merge">
+                        <div class="form-floating form-floating-outline">
+                            <input type="password" id="password"
+                                class="form-control @error('password') is-invalid @enderror" name="password"
+                                placeholder="Password" autocomplete="current-password" required
+                                aria-describedby="password" />
+                            <label for="password">Password</label>
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        <span class="input-group-text cursor-pointer"><i class="ri-eye-off-line"></i></span>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="mb-5">
+                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+            </div>
+        </form>
     </div>
 </div>
+<!-- /Login -->
+<img alt="mask"
+    src="{{ asset('assets/img/illustrations/auth-basic-login-mask-light.png') }}?v={{ config('app.version') }}"
+    class="authentication-image d-none d-lg-block" />
+@endsection
+
+@section('js')
 @endsection
