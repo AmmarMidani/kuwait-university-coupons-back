f117aa2f62330448a6ea7fe1cf3cafd310534fd4
Ammar Midani <ammar.midani.1994@gmail.com>
2025-05-27 22:08:34 +0300

    add blank page

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('pagename')
+Blank Page
+@endsection
+
+@section('css_plugin')
+@endsection
+
+@section('css')
+@endsection
+
+@section('content')
+@endsection
+
+@section('js_plugin')
+@endsection
+
+@section('js')
+@endsection
