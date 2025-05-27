660d7c6a697c2aa79b30e32f24fb79f5f579c0e1
Ammar Midani <ammar.midani.1994@gmail.com>
2025-05-27 22:08:40 +0300

    add home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
+@section('pagename')
+Home page
+@endsection
+
+@section('css_plugin')
+@endsection
+
+@section('css')
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+@endsection
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('js_plugin')
+@endsection
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('js')
 @endsection
