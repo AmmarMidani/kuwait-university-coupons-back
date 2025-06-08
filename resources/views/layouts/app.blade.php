39b74b5237e102ed9121c5aef4c909192c50200c
Ammar Midani <ammar.midani.1994@gmail.com>
2025-06-08 14:49:58 +0300

    add jquery validator + select2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,7 @@
     <link rel="stylesheet"
         href="{{ asset('assets/vendor/libs/@form-validation/form-validation.css') }}?v={{ config('app.version') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/swiper/swiper.css') }}?v={{ config('app.version') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}?v={{ config('app.version') }}" />
 
     <!-- Page CSS -->
     <link rel="stylesheet"
@@ -153,6 +154,7 @@
     </script>
     <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}?v={{ config('app.version') }}"></script>
     <script src="{{ asset('assets/vendor/libs/swiper/swiper.js') }}?v={{ config('app.version') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}?v={{ config('app.version') }}"></script>
 
     <!-- Main JS -->
     <script src="{{ asset('assets/js/main.js') }}?v={{ config('app.version') }}"></script>
