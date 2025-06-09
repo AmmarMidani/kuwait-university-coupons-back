c44a153bafe64aba2bd9cf7b96fc21def75e2423
Ammar Midani <ammar.midani.1994@gmail.com>
2025-06-09 16:49:47 +0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['register' => false, 'password.request' => false, 'reset' => false]);
 
+Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
+
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
     Route::resource('user', UserController::class)->name('*', 'user');
