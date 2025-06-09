8a7a50e5bb5cccdea0ab9f45bf5f62e11a73cae2
Ammar Midani <ammar.midani.1994@gmail.com>
2025-06-09 22:56:52 +0300

    fix footer Copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-xxl">
         <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
             <div class="text-body mb-2 mb-md-0">
-                Copyright © 2022 |
+                Copyright © 2025 |
                 <a href="https://www.ku.edu.kw/" target="_blank" class="footer-link">
                     Kuwait University
                 </a>
