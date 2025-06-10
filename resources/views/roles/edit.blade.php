e5a02e505099d306058c969e717a21900c22b4df
Ammar Midani <ammar.midani.1994@gmail.com>
2025-06-10 04:00:03 +0300

    bug fixes

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -15,7 +15,7 @@ Roles & Permissions
     class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center mb-6 row-gap-4">
     <div class="d-flex flex-column justify-content-center">
         <h4 class="mb-1">Roles & Permissions</h4>
-        <p class="mb-0">Create new Roles & Permissions</p>
+        <p class="mb-0">Update Roles & Permissions details</p>
     </div>
     <div class="d-flex align-content-center flex-wrap gap-4">
         <!-- action buttons -->
