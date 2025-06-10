7f3946c7a7219347396e55dac1765f8509fdab57
Ammar Midani <ammar.midani.1994@gmail.com>
2025-06-11 01:59:59 +0300

    add sweetalert2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,9 @@
         href="{{ asset('assets/vendor/libs/@form-validation/form-validation.css') }}?v={{ config('app.version') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/swiper/swiper.css') }}?v={{ config('app.version') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}?v={{ config('app.version') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}?v={{ config('app.version') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('assets/vendor/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}?v={{ config('app.version') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.css') }}?v={{ config('app.version') }}" />
 
     <!-- Page CSS -->
     <link rel="stylesheet"
@@ -166,8 +168,10 @@
     <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}?v={{ config('app.version') }}"></script>
     <script src="{{ asset('assets/vendor/libs/swiper/swiper.js') }}?v={{ config('app.version') }}"></script>
     <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}?v={{ config('app.version') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js') }}?v={{ config('app.version') }}"></script>
-
+    <script
+        src="{{ asset('assets/vendor/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js') }}?v={{ config('app.version') }}">
+    </script>
+    <script src="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.js') }}?v={{ config('app.version') }}"></script>
     <!-- Main JS -->
     <script src="{{ asset('assets/js/main.js') }}?v={{ config('app.version') }}"></script>
 
